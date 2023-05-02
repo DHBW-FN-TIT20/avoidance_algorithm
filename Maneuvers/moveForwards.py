c27dd2b0e3bb19dc1f5396780311dd27e1df0b00
LukasAdrion <Lukas@Adrion.com>
2023-05-02 13:33:43 +0000

    :sparkles: add lane correction in moveForward()

--- Maneuvers/moveForwards.py
+++ Maneuvers/moveForwards.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
 from ev3dev2.motor import *
+from ev3dev2.sensor.lego import GyroSensor
+from ev3dev2.sensor import INPUT_2
 
-lm = LargeMotor(OUTPUT_A)
+drive = LargeMotor(OUTPUT_A)
+steering = LargeMotor(OUTPUT_B)
+gyro = GyroSensor(INPUT_2)
+
+gyro.calibrate()
+gyro.reset()
+steering.position = 0
 
 def moveForwards():
-	lm.on(SpeedDPS(90), True)
+	while True:
+		drive.on(SpeedDPS(90), True)
+		if gyro.angle < -5:
+			steering.on_for_degrees(speed=SpeedDPS(90), degrees=-30, brake=True, block=False)
+			while gyro.angle < 0:
+                                print(str(gyro.angle))
+			steering.on_for_degrees(speed=SpeedDPS(90), degrees=((steering.position)*-1)+5, brake=True, block=False)
+		if gyro.angle > 5:
+			steering.on_for_degrees(speed=SpeedDPS(90), degrees=30, brake=True, block=False)
+			while gyro.angle > 0:
+				print(str(gyro.angle))
+			steering.on_for_degrees(speed=SpeedDPS(90), degrees=((steering.position)*-1)+5, brake=True, block=False)
 
 moveForwards()
