f2b36deab2fcc3d18f1c723f8cfea0903dc756f4
LukasAdrion <Lukas@Adrion.com>
2023-05-13 20:39:21 +0000

    :package: add getAngleOfObject.py file

---
+++ ObjectDetection/getAngleOfObject.py
@@ -0,0 +1,8 @@
+from ev3dev2.sensor.lego import UltrasonicSensor
+from ev3dev2.motor import *
+from ev3dev2.sensor import INPUT_1
+
+us = UltrasonicSensor()
+usMotor = LargeMotor(OUTPUT_C)
+
+def getAngleOfObject:
