6ec55bb9215b78950e2004d026342ad797d4223c
LukasAdrion <Lukas@Adrion.com>
2023-06-02 16:48:49 +0000

    :sparkles: add offset for 90 and -90 degrees

--- trackOffset.py
+++ trackOffset.py
@@ -18,6 +18,7 @@ gyro.reset()
 
 offset = 0
 oldAngle = gyro.angle
+positive = None
 
 # calculates the distance of the "opposite side" with sinus
 def calculateOffset(angle):
@@ -26,10 +27,26 @@ def calculateOffset(angle):
 	offset = math.sin(math.radians(angle)) * distance
 	return offset
 
+drive.on(SpeedDPS(90), True)
 while True:
 	newAngle = gyro.angle
+
+	if newAngle == 90:
+		drive.position = 0
+		while gyro.angle == 90:
+			print(gyro.angle)
+		print("---------------- offset: " + str(offset))
+		offset = offset + (drive.position / 27.4)
+		print("---------------- new offset: " + str(offset))
+
+	if newAngle == -90:
+                drive.position = 0
+                while gyro.angle == -90:
+                        print(gyro.angle) 
+                offset = offset - (drive.position / 27.4)
 
 	if newAngle != oldAngle:
 		offset = offset + calculateOffset(newAngle)
 		oldAngle = newAngle
 	print("old angle: " + str(oldAngle) + ", offset: " + str(offset))
+
